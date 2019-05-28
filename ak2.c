a81f49275705487efa9b9b0544185c9e63492e1d
Marcin Nowak <marcinnowak759@gmail.com>
2019-05-28 15:53:44 +0200

    add random number generator

--- ak2.c
+++ ak2.c
@@ -1,20 +1,57 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define buffer_size 1024
 
 extern void multiplication();
 
+
+char* random_number(){
+	static char bit_array[buffer_size];
+	int number_size = rand() % buffer_size;
+	int i;
+	for(i = 0; i < number_size; i++){
+		bit_array[i] = (rand() % 2) +  '0';
+	}
+	bit_array[i] = 0;
+
+	return bit_array;
+}
+
+void generate_random_file(char* filename){
+	char* generated_number = random_number();
+	FILE* fd;
+	fd = fopen(filename, "w");
+	fprintf(fd, "%s", generated_number);
+	fclose(fd);
+}
+
 int main(int argc, const char* argv[]){
-char choice;
+	time_t tt;
+	int seed = time(&tt);
+	srand(seed);
+char choice = 1;
 	printf("1. Uruchom program\n"
+		"2. Generuj losowe dane\n"
 			"0. Wyjscie\n");
 
-	scanf("%c", &choice);
 
-	switch(choice){
-		case '1':
-			multiplication();
-			break;
-		default:
-			break;
+	do{
+	scanf("%c", &choice);
+		switch(choice){
+			case '1':
+				multiplication();
+				break;
+			case '2':
+				generate_random_file("in1_binary");
+				generate_random_file("in2_binary");
+			default:
+				break;
+		}
 	}
+	while (choice != '0');
+
 	return 0;
 }
+
